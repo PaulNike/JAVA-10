78ddb95956ef57bf8abf23444fd70c95d85fc93b
NikeRodriguez <nike_1109@outlook.es>
2025-04-28 23:17:41 -0500

    Tema: Retrofit - Completo

--- retrofit/src/main/java/com/codigo/retrofit/RetrofitApplication.java
+++ retrofit/src/main/java/com/codigo/retrofit/RetrofitApplication.java
@@ -1,5 +1,6 @@
 package com.codigo.retrofit;
 
+import com.codigo.retrofit.aggregates.ConexionDb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,11 @@ public class RetrofitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetrofitApplication.class, args);
+
+		ConexionDb con1 = ConexionDb.obtenerInstancia();
+		ConexionDb con2 = ConexionDb.obtenerInstancia();
+
+		System.out.println(con1 == con2);
 	}
 
 }
