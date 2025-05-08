54e7aa8a03720f7a74a8243cd0b015250fb65b82
NikeRodriguez <nike_1109@outlook.es>
2025-05-07 19:37:27 -0500

    Tema: Implementando Arquetipo Hexagonal

---
+++ ms-register-hexagonal/src/test/java/com/codigo/msregisterhexagonal/MsRegisterHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigo.msregisterhexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegisterHexagonalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
